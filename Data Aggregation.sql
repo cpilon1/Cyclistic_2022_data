7b5333cd484a8442540c75aa95c6496d68301451
Crystal Pilon <144136275+cpilon1@users.noreply.github.com>
2023-09-28 15:07:46 -0400

    Update Data Aggregation.sql

--- Data Aggregation.sql
+++ Data Aggregation.sql
@@ -37,3 +37,15 @@ FROM `first-project-394620.Cyclistic_2022.data_11`
 UNION ALL
 SELECT*
 FROM `first-project-394620.Cyclistic_2022.data_12`
+
+-- Create a table of station name and average latitude and longitude. -- 
+CREATE TABLE first-project-394620.Cyclistic_2022.station_coords1 AS
+SELECT
+start_station_name,
+ROUND(AVG(start_lat),3) AS station_lat,
+ROUND(AVG(start_lng),3) AS station_lng
+FROM `first-project-394620.Cyclistic_2022.data_aggregated`
+WHERE
+start_lng IS NOT NULL AND start_station_name IS NOT NULL
+GROUP BY start_station_name
+ORDER BY start_station_name
